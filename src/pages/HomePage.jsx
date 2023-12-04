28a3b1e474fbf0a001f6575dab3593e4929118ba
ruchipratihast <ruchipratihast99@gmail.com>
2023-12-04 23:27:15 +0530

    added all notes section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,6 +18,7 @@ const HomePage = () => {
               <div>
                 <ProfileInfo/>
               </div>
+              
               <div>
                 <Datetime/>
                 <Weather/>
